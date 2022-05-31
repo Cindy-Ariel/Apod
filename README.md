e18e75a2399322d89a8327c66b523e908b9ee9e5
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-05-30 22:53:53 -0300

    Construindo CSS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Apod
- Mini projeto para busca de imagens e vídeos da NASA - Astronomy Picture of the Day
+Mini projeto para busca de imagens e vídeos da NASA - Astronomy Picture of the Day 
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-$("#botao").click(() => {
-  const dataSelecionada = $("#data").val();
+const titulo = $("#titulo");
+const tagImagemAPI = $("#imagem");
+const tagVideo = $("#video");
+const tagTexto = $("#texto");
+const tagBotao = $("#botao");
+const imputData = $("#data");
 
+tagVideo.hide();
+tagImagemAPI.hide();
+tagTexto.hide();
+
+tagBotao.click(() => {
+  const dataSelecionada = $(imputData).val();
+  $("body").css("background-image", "url('image/imgfundo.jpeg')");
   $.get(
     `https://api.nasa.gov/planetary/apod?api_key=FEj5l4GkuixWAH6qkkahRoexHq0q4Lculo4Co9GN&date=${dataSelecionada}`,
     (resultado) => {
@@ -9,13 +20,18 @@ $("#botao").click(() => {
   )
     .done((resultado) => {
       if (resultado.media_type === "image") {
-        $("#imagem").attr("src", resultado.url);
+        tagImagemAPI.attr("src", resultado.url).show();
+        tagVideo.hide();
       } else if (resultado.media_type === "video") {
-        $("#video").attr("src", resultado.url);
+        tagVideo.attr("src", resultado.url).show();
+        tagImagemAPI.hide();
       }
+      titulo.text(resultado.title).show();
+      tagTexto.text(resultado.explanation).show();
     })
+
     .fail(() => {
-      $("#imagem").attr(
+      tagImagemAPI.attr(
         "src",
         "https://cdn.pixabay.com/photo/2021/07/21/12/49/error-6482984_960_720.png"
       );
